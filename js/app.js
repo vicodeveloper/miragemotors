512de0c1c0c240e30ed9473938b8bb1f0e0d6966
vicodeveloper <ecsmurder@gmail.com>
2016-11-30 18:52:14 +0200

    MirageMotors 30.11.2016

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ViCODeveloper on 025 25.11.16.
+ */
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+
+//slider activate
+$('.slider').slider({
+    full_width: true,
+    height: 450,
+    indicators: false,
+    interval:3000
+});
+
+$('.carousel').carousel({
+    full_width:true,
+    padding:30,
+    indicators:false,
+    shift: 3
+});
